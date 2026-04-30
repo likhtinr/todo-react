9537c5701385438214e533c2a8cc40cf1879bc3b
likhtinr <isif-blog@yandex.ru>
2026-04-30 21:20:54 +0300

    Исправил роутер для GitHub Pages

--- src/shared/api/tasks/index.js
+++ src/shared/api/tasks/index.js
@@ -1,8 +1,6 @@
 import localAPI from "./local";
 import serverAPI from "./server";
 
-console.log("Моя переменная окружения:", import.meta.env.VITE_STATIC_BACKEND);
-
 const isLocal = import.meta.env.VITE_STATIC_BACKEND === "true";
 
 const tasksAPI = isLocal ? localAPI : serverAPI;
